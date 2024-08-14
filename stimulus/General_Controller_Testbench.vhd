0d6a63d38bd73dd1b95c679e8496207370c7cf56
Rudolfs Arvids Kalnins <rakal@kth.se>
2024-08-14 23:20:51 +0300

    FPGA uC message handling testbench

--- stimulus/General_Controller_Testbench.vhd
+++ stimulus/General_Controller_Testbench.vhd
@@ -41,8 +41,26 @@ architecture behavioral of General_Controller_Testbench is
     signal UC_RX_RDY : std_logic := '1';
     signal UC_RECV : std_logic_vector(7 downto 0):= x"B5";
     
-    type b_array is array (0 to 15) of std_logic_vector(7 downto 0);
-    signal SEND_ARRAY : b_array := (x"B5", x"43", x"AB", x"0B", x"01", x"02", x"03", x"04", x"05", x"06", x"07", x"08", x"09", x"0A", x"0B", x"0A");
+    type AB_array is array (0 to 7) of std_logic_vector(7 downto 0);
+    signal SEND_AB_ARRAY : AB_array := (x"B5", x"43", x"AB", x"00", x"30", x"CD", x"AB", x"0A");
+
+    type CB_array is array (0 to 14) of std_logic_vector(7 downto 0);
+    signal SEND_CB_ARRAY : CB_array := (x"B5", x"43", x"CB", x"01", x"CD", x"AB", x"0A", x"00", x"B5", x"43", x"CB", x"00", x"FD", x"CE", x"0A");
+
+    type AA_array is array (0 to 3) of std_logic_vector(7 downto 0);
+    signal SEND_AA_ARRAY : AA_array := (x"B5", x"43", x"AA", x"0A");
+
+    type CA_array is array (0 to 3) of std_logic_vector(7 downto 0);
+    signal SEND_CA_ARRAY : CA_array := (x"B5", x"43", x"CA", x"0A");
+
+    type AC_array is array (0 to 4) of std_logic_vector(7 downto 0);
+    signal SEND_AC_ARRAY : AC_array := (x"b5", x"43", x"ac", x"bc", x"0a");
+
+    type AD_array is array (0 to 4) of std_logic_vector(7 downto 0);
+    signal SEND_AD_ARRAY : AD_array := (x"b5", x"43", x"ad", x"20", x"0a");
+
+
+
 
     signal unit_id : std_logic_vector(7 downto 0);
     signal ffu_id : std_logic_vector(7 downto 0);
@@ -138,10 +156,10 @@ begin
             NSYSRESET <= '0';
             vhdl_initial := FALSE;
         else
-            for i in SEND_ARRAY'range loop
+            for i in SEND_CB_ARRAY'range loop
                 wait for (SYSCLK_PERIOD * 2);
                 UC_RX_RDY <= '1';                
-                UC_RECV <= SEND_ARRAY(i);
+                UC_RECV <= SEND_CB_ARRAY(i);
                 wait for (SYSCLK_PERIOD * 2);
                 UC_RX_RDY <= '0';
             end loop;
