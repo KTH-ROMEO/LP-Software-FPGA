6883cf851fb6f64f390d1542c8ec3089b545f6af
Sergiu Bogdan Popescu <sergiupopescu22@gmail.com>
2025-03-13 22:00:25 +0100

    deleted more unncessary components, tested functionality with JULIET

--- designer/impl1/Toplevel_fp/Toplevel.tcl
+++ designer/impl1/Toplevel_fp/Toplevel.tcl
@@ -1,4 +1,4 @@
-open_project -project {C:\Users\sergi\Desktop\Master Year 2\Master Thesis\20250312_LP-Software-FPGA\designer\impl1\Toplevel_fp\Toplevel.pro}
+open_project -project {C:\Users\sergi\Desktop\Master Year 2\Master Thesis\LP-Software-FPGA\designer\impl1\Toplevel_fp\Toplevel.pro}
 set_programming_action -action {PROGRAM}
 run_selected_actions
 save_project
--- synthesis/Toplevel_syn.tcl
+++ synthesis/Toplevel_syn.tcl
@@ -1,3 +1,3 @@
-project -load "C:/Users/sergi/Desktop/Master Year 2/Master Thesis/20250312_LP-Software-FPGA/synthesis/Toplevel_syn.prj"
+project -load "C:/Users/sergi/Desktop/Master Year 2/Master Thesis/LP-Software-FPGA/synthesis/Toplevel_syn.prj"
 project -run -clean
 project -save
