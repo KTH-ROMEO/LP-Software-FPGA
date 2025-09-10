9fa9212235c01521127158610e81548f2d705cf6
jNieto98 <jesus.nieto.rojo@gmail.com>
2025-09-10 19:37:05 +0200

    Fix .gitIgnore Issue

--- designer/impl1/Toplevel.tcl
+++ designer/impl1/Toplevel.tcl
@@ -1,5 +1,5 @@
 # Created by Microsemi Libero Software 11.9.6.7
-# Fri Sep 05 18:53:31 2025
+# Wed Sep 10 19:26:53 2025
 
 # (OPEN DESIGN)
 
@@ -14,9 +14,13 @@ set_defvar "IDE_DESIGNERVIEW_REVNUM0" "1"
 set_defvar "IDE_DESIGNERVIEW_ROOTDIR" {C:\Users\Jesus\Documents\KTH\ROMEO\Repositories\LP-Software-FPGA\designer}
 set_defvar "IDE_DESIGNERVIEW_LASTREV" "1"
 
-report -type "timing" -format "TEXT" -analysis "max" -print_summary "yes" -use_slack_threshold "no"                             -print_paths "yes" -max_paths 5 -max_expanded_paths 1                             -max_parallel_paths 1 -include_user_sets "no"                             -include_pin_to_pin "yes" -include_clock_domains "yes"                             -select_clock_domains "no" {Toplevel_maxdelay_timing_report.txt}
-report -type "timing" -format "TEXT" -analysis "min" -print_summary "yes" -use_slack_threshold "no"                             -print_paths "yes" -max_paths 5 -max_expanded_paths 1                             -max_parallel_paths 1 -include_user_sets "no"                             -include_pin_to_pin "yes" -include_clock_domains "yes"                             -select_clock_domains "no" {Toplevel_mindelay_timing_report.txt}
-report -type "timing_violations" -format "TEXT" -analysis "max" -use_slack_threshold "yes" -slack_threshold 0.00                               -limit_max_paths "yes" -max_paths 100 -max_expanded_paths 0                               -max_parallel_paths 1 {Toplevel_maxdelay_timingviolations_report.txt}
-report -type "timing_violations" -format "TEXT" -analysis "min" -use_slack_threshold "yes" -slack_threshold 0.00                               -limit_max_paths "yes" -max_paths 100 -max_expanded_paths 0                               -max_parallel_paths 1 {Toplevel_mindelay_timingviolations_report.txt}
+
+layout -timing_driven
+report -type "status" {Toplevel_place_and_route_report.txt}
+report -type "globalnet" {Toplevel_globalnet_report.txt}
+report -type "globalusage" {Toplevel_globalusage_report.txt}
+report -type "iobank" {Toplevel_iobank_report.txt}
+report -type "pin" -listby "name" {Toplevel_report_pin_byname.txt}
+report -type "pin" -listby "number" {Toplevel_report_pin_bynumber.txt}
 
 save_design
